a1725d16f4d91a41dcb565ee7055d1af7e652944
Stephen <tremaine.stephen@gmail.com>
2025-05-03 12:24:48 -0500

    Svelte config -> Static

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/convergence' : ''
-		}
-	}
+    preprocess: vitePreprocess(),
+    
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/convergence' : ''
+        }
+    }
 };
 
 export default config;
